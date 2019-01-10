79f7f53a2da15deb15b2261b3310bcd82f3a264b
Jordan Earls <earlz@earlz.net>
2019-01-10 14:26:45 -0700

    Fix some errors in utils and env vars

--- proto-x86/helpers.sh
+++ proto-x86/helpers.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-export STARTQTUM docker run --rm -v "${PWD}:/root/bind" --name qx86 -d qtumx86 qtum/src/qtumd -regtest -logevents
-export RESETQTUM docker rm -f qx86 && $STARTQTUM
-export STOPQTUM docker stop qx86
-export QCLI docker exec qx86 qcli
+export STARTQTUM="docker run --rm -v ""${PWD}:/root/bind"" --name qx86 -d qtumx86 qtum/src/qtumd -regtest -logevents"
+export RESETQTUM="docker rm -f qx86 && $STARTQTUM"
+export STOPQTUM='docker stop qx86'
+export QCLI='docker exec qx86 qcli'
 
 function qtumdeploy() {
     docker exec -t qx86 deploy_contract `hexdump -e \"%x\" $1` 
 }
 export -f qtumdeploy
 
-export X86TB docker run --rm -v "${PWD}:/root/bind" qtumx86 x86tb
-export QMAKE docker run --rm -v "${PWD}:/root/bind" qtumx86 qmake
+export X86TB="docker run --rm -v ""${PWD}:/root/bind"" qtumx86 x86tb"
+export QMAKE="docker run --rm -v ""${PWD}:/root/bind"" qtumx86 qmake"
