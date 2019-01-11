b15a379777938a27e13c8154b8928be00e1c53e3
Jordan Earls <earlz@earlz.net>
2019-01-10 18:12:07 -0700

    Rework docker and aliases

--- proto-x86/helpers.sh
+++ proto-x86/helpers.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
-export STARTQTUM="docker run --rm -v ""${PWD}:/root/bind"" --name qx86 -d qtumx86 qtum/src/qtumd -regtest -logevents"
-#export RESETQTUM="docker rm -f qx86 && $STARTQTUM"
-export STOPQTUM='docker stop qx86'
-export QCLI='docker exec qx86 qcli'
+function qx86start() {
+    docker run --rm -v "${PWD}:/root/bind" --name qx86 -d qtumx86 qtum/src/qtumd -regtest -logevents
+}
+export -f qx86start
 
-function qtumdeploy() {
+function qx86stop() {
+    docker stop qx86
+}
+export -f qx86stop
+
+alias qx86cli='docker exec qx86 qcli'
+
+function qx86deploy() {
     docker exec -t qx86 deploy_contract `hexdump -e \"%x\" $1` 
 }
 export -f qtumdeploy
 
-export X86TB="docker run --rm -v ""${PWD}:/root/bind"" qtumx86 x86tb"
-export QMAKE="docker run --rm -v ""${PWD}:/root/bind"" qtumx86 qmake"
+function qx86tb() {
+    docker run --rm -v "${PWD}:/root/bind" qtumx86 x86tb
+}
+export -f qx86tb
+
+function qx86make() {
+    docker run --rm -v "${PWD}:/root/bind" qtumx86 qmake "$@"
+}
+export -f qx86make
