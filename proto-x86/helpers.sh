406dc19cf1fbd69e8100a349b94d16331ee9c3a5
Jordan Earls <earlz@earlz.net>
2019-01-10 17:49:27 -0700

    remove QTUMRESET for now

--- proto-x86/helpers.sh
+++ proto-x86/helpers.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export STARTQTUM="docker run --rm -v ""${PWD}:/root/bind"" --name qx86 -d qtumx86 qtum/src/qtumd -regtest -logevents"
-export RESETQTUM="docker rm -f qx86 && $STARTQTUM"
+#export RESETQTUM="docker rm -f qx86 && $STARTQTUM"
 export STOPQTUM='docker stop qx86'
 export QCLI='docker exec qx86 qcli'
 
