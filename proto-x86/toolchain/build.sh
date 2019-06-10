20618700ef9c2306825bf96b8713a1403babaa58
VoR0220 <catalanor0220@gmail.com>
2019-06-10 16:03:47 -0500

    split toolchain from main dockerfile
    Signed-off-by: VoR0220 <catalanor0220@gmail.com>

--- proto-x86/toolchain/build.sh
+++ proto-x86/toolchain/build.sh
@@ -1,4 +1,3 @@
-docker build --no-cache -f Dockerfile.dev -t alpine-dev .
-docker build --no-cache -f Dockerfile.build -t qtumtoolchain-alpine-build .
-docker build --no-cache -f Dockerfile.install -t qtumtoolchain-alpine-bin .
-#docker build -f Dockerfile -t qtum-toolchain-alpine .
+docker build -f Dockerfile.dev -t alpine-dev .
+docker build -f Dockerfile.build -t qtumtoolchain-alpine-build .
+docker build -f Dockerfile -t qtumtoolchain-alpine .
